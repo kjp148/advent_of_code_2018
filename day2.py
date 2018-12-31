7ca0df44d29f864f8b716cd8254810ee18431f1d
kjp148 <keananpaul@yahoo.com>
2018-12-31 02:59:38 -0800

    Finished day 2, made output more readable

--- day2.py
+++ day2.py
@@ -1,11 +1,11 @@
-file = open("files/day2input.txt")
+with open("files/day2input", "r") as file:
+    box_IDs = [str(box_ID) for box_ID in file]
 
 # Part 1
 two_sum   = 0
 three_sum = 0
 
-for line in file:
-
+for line in box_IDs:
     seen       = {}
     two_flag   = False
     three_flag = False
@@ -33,8 +33,33 @@ for line in file:
 
 checksum = two_sum * three_sum
 
+print("< Day 2 Part 1 >")
 print("Two Sum:   " + str(two_sum))
 print("Three Sum: " + str(three_sum))
-print("Checksum:  " + str(checksum))
+print("Checksum:  " + str(checksum) + "\n")
 
-file.close()
+# Part 2
+for current_ID in box_IDs:
+    for check_ID in box_IDs:
+        different = 0
+
+        for i in range(len(current_ID) - 1):
+            if current_ID[i] is check_ID[i]:
+                different += 0
+
+            else:
+                different += 1
+
+        if different is 1:
+            current_ID = current_ID.replace('\n', '')
+            check_ID   = check_ID.replace('\n', '')
+
+            for i in range(len(current_ID) - 1):
+                if current_ID[i] is not check_ID[i]:
+                    correct_ID = current_ID[:i] + current_ID[i + 1:]
+
+            print("< Day 2 Part 2 >")
+            print("Current ID:   " + current_ID)
+            print("Identical ID: " + check_ID)
+            print("Correct ID:   " + correct_ID + "\n")
+            exit()
