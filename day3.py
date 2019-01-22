a8b41819c3be796890b4e612d1463cd29c02f7d2
kjp148 <keananpaul@yahoo.com>
2019-01-22 01:24:57 -0800

    Removed extra debugging output, fixed naming of instr_array list

--- day3.py
+++ day3.py
@@ -17,22 +17,20 @@ class Instruction:
         print("Width:        " + str(self.size_horiz))
         print("Height:       " + str(self.size_vert))
 
-instr_array = []
+instr_list = []
 for line in file:
     ID          = line[line.find("#") + 1:line.find(" ")]  # After "#"  | until " "
     from_left   = line[line.find("@ ") + 2:line.find(",")] # After "@ " | until ","
     from_top    = line[line.find(",") + 1:line.find(":")]  # After ","  | until ":"
     size_horiz  = line[line.find(": ") + 2:line.find("x")] # After ": " | until "x"
     size_vert   = line[line.find("x") + 1:]                # After "x"  | until end of line
-    instr_array.append(Instruction(ID, from_left, from_top, size_horiz, size_vert))
-
-for i in instr_array:
-    i.printAll()
+    instr_list.append(Instruction(ID, from_left, from_top, size_horiz, size_vert))
 
 # Part 1
 sheet = [[0] * 1000 for i in range(1000)]
 
-for instruction in instr_array:
+# Create list of instructions
+for instruction in instr_list:
     horiz_start = instruction.from_left - 1
     horiz_end   = horiz_start + instruction.size_horiz
     vert_start  = instruction.from_top - 1
