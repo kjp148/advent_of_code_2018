f01509d9013f7af7e6ea6584fda26440c0241b4f
kjp148 <keananpaul@yahoo.com>
2019-01-29 06:40:20 -0800

    Finished part 1 with incorrect output

--- day4.py
+++ day4.py
@@ -17,6 +17,24 @@ class Log:
         full_date = str(self.date) + str(self.hour) + str(self.minute)
         return full_date
 
+class Guard:
+    asleep = [0] * 60 # One index for each minute in the midnight hour
+
+    def __init__ (self, ID):
+        self.ID = ID
+
+    def printAsleep (self):
+        asleep_string = ""
+        for minute, amount in enumerate(self.asleep):
+            asleep_string += " | " + str(minute) + ": " + str(amount)
+        return asleep_string
+
+    def totalMinutes (self):
+        minute_total = 0
+        for amount in self.asleep:
+            minute_total += amount
+        return minute_total
+
 # Create list of objects for sorting
 log_list = []
 for line in file:
@@ -42,7 +60,10 @@ for log in log_list:
 # Part 1
 
 # Gather list of each guard's activities (guard # = index)
-guard_str_list = [""] * (max_guard_number + 1)
+guard_list = []
+for i in range(max_guard_number + 1):
+    guard_list.append(Guard(i + 1))
+
 for log in log_list:
     if "Guard" in log.entry:
         str_start    = log.entry.find("#") + 1
@@ -50,10 +71,27 @@ for log in log_list:
         guard_number = int(log.entry[str_start:str_end])
 
     elif "falls" in log.entry:
-        guard_str_list[guard_number] += str(log.hour) + str(log.minute) + "-"
+        start_sleep = int(log.minute)
 
     elif "wakes" in log.entry:
-        guard_str_list[guard_number] += str(log.hour) + str(log.minute) + ","
+        end_sleep = int(log.minute)
+        for i in range(start_sleep, end_sleep):
+            guard_list[guard_number].asleep[i] += 1
 
-for index, guard in enumerate(guard_str_list):
-    print("Guard " + str(index) + ": " + guard)
+# Find guard with most minutes asleep
+highest_guard = guard_list[0]
+for guard in guard_list:
+    if guard.totalMinutes() > highest_guard.totalMinutes():
+        highest_guard = guard
+
+# Find minute most asleep
+minute_max = highest_guard.asleep[0]
+for minute in highest_guard.asleep:
+    if minute > minute_max:
+        minute_max = minute
+
+print("< Day 4 Part 1 >")
+print("Guard Most Asleep:    " + str(highest_guard.ID))
+print("Total Minutes Asleep: " + str(highest_guard.totalMinutes()))
+print("Minute Most Aleep:    " + str(minute_max))
+print("Output:               " + str(highest_guard.ID * minute_max))
